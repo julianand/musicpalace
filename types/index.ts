752c48582655a8a80c421050345de3ca9fade8c8
Julian Pitre <julianpitre1997@gmail.com>
2026-08-21 23:25:30 -0500

    add favorite schema

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,15 @@
-import type { products, product_categories, reviews, users } from "@/app/generated/prisma/client"
+import type { products, product_categories, reviews, users, favorites } from "@/app/generated/prisma/client"
 
 // export type Product = products;
 export type ProductCategory = product_categories;
 // export type Review = reviews;
 export type User = users;
+export type Favorite = favorites;
 
 export type Product = products & {
   category: ProductCategory;
   formattedPrice: string;
+  favorite?: boolean;
 }
 
 export type Review = reviews & {
